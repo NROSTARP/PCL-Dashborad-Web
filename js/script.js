e13fd691c68488f98bc36f10fe7e3429db2bcca7
NROSTARP <nrostarp0765@gmail.com>
2022-06-27 16:50:49 +0700

    Dashboard Done. Working on Sample  Page element Table for Data

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// $('.nav-toggle').click(function(e) {
+  
+//     e.preventDefault();
+//     $("html").toggleClass("openNav");
+//     $(".nav-toggle").toggleClass("active");
+  
+//   });
+
+  //---------------------------------------- Time function -----------------------------------------//
+
+var timeDisplay = document.getElementById("time");
+
+function refreshTime() {
+  var dateString = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
+  var formattedString = dateString.replace(", ", " - ");
+  timeDisplay.innerHTML = formattedString;
+}
+
+setInterval(refreshTime, 1000);
